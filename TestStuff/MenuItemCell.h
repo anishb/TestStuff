39e8dcd21af52c953b49145a8a9d8f1f82585e0d
Anish Basu <anish.basu@cbsinteractive.com>
2014-02-28 15:37:08 -0800

    Added table view

---
+++ TestStuff/MenuItemCell.h
@@ -0,0 +1,15 @@
+//
+//  MenuItemCell.h
+//  TestStuff
+//
+//  Created by Anish Basu on 2/28/14.
+//  Copyright (c) 2014 Anish Basu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuItemCell : UITableViewCell
+
+@property (nonatomic, copy) NSString *menuTitle;
+
+@end
--- TestStuff/MenuViewController.m
+++ TestStuff/MenuViewController.m
@@ -29,16 +29,6 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor clearColor];
-    CGFloat red, green, blue, alpha;
-    if ([[UIColor redColor] getRed:&red green:&green blue:&blue alpha:&alpha]) {
-        NSLog(@"R:%f G:%f B:%f a:%f", red, green, blue, alpha);
-    }
-    
-    
-    //self.blurView.blurTintColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0.65];
-    //self.blurView.blurTintColor = [UIColor grayColor];
-    //self.blurView.alpha = 1.0;
-    //self.blurView.opaque = NO;
 }
 
 - (void)didReceiveMemoryWarning
